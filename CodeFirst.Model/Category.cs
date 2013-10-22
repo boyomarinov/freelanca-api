6dadb2be617f26720f609551a2f42ad094642b6a
Boyan Marinov <boyan.marinov@yahoo.com>
2013-10-23 00:20:36 +0300

    api init

---
+++ CodeFirst.Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeFirst.Model
+{
+    public enum Category
+    {
+        Other,
+        Cat1,
+        Cat2,
+        Cat3,
+        Cat4
+    }
+}
---
+++ CodeFirst.Model/User.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CodeFirst.Model;
+
+namespace CodeFirst.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        #region UserType   STRING: Username, DisplayName, AuthCode, AccessToken, Mail, Phone, Location
+        [Required]
+        [MinLength(6)]
+        [MaxLength(30)]
+        [StringLength(30)]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(30)]
+        [StringLength(30)]
+        public string DisplayName { get; set; }
+
+        [MinLength(40)]
+        [MaxLength(40)]
+        [StringLength(40)]
+        public string AuthCode { get; set; }
+
+        [MinLength(50)]
+        [MaxLength(50)]
+        [StringLength(50)]
+        public string AccessToken { get; set; } 
+        
+        public UserType UserType { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Location { get; set; }
+        #endregion
+        
+        public virtual ICollection<Job> OwnRequests { get; set; } 
+        public virtual ICollection<Job> Jobs { get; set; }
+
+
+        public User()
+        {
+            this.OwnRequests = new HashSet<Job>();
+            this.Jobs = new HashSet<Job>();
+        }
+    } 
+}
